9059be52bf16ba885e4b808ac3f7428741fa03e9
Abdullah Alrjoub <aaa4822@rit.edu>
2021-10-31 11:37:09 +0400

    parameters now input

--- .github/plots_cli.py
+++ .github/plots_cli.py
@@ -4,79 +4,88 @@ import plotter
 
 
 def main():
-    def student_average(param):
-            param = ["stu", r"C:\Users\abdul\Downloads\GCIS.123.600-assignment2-sample.csv", "Altamimi",'Abdullah']
+        def print_average(string):
+                string = input('enter ')
+
+                #quit function
+        def quits():
+                sure = input("are you sure?")
+                while sure == 'y':
+                    print('goodbye!')
+                    return True
+                while sure == 'Y':
+                    print('goodbye!')
+                    return True
+                else:
+                    return False
+        def bye():
+            user = input(">>")
+            string = user.split()
+            cmd = string[0]
+            if cmd == "quit":
+                    while quits() == True:
+                        break
+            elif cmd == 'stu':
+                    try:
+                        if len(string) != 4:
+                            print('incorrect usage, "Usage: stu <filename> <last name> <first name>"')
+                        else:
+                            userfile = string[1]
+                            first_name= string[3]
+                            last_name= string[2]
+                            param = ["stu", r"C:\Users\abdul\Downloads\GCIS.123.600-assignment2-sample.csv", "Altamimi", "Abdullah"]
+                            student_average(["stu", userfile, last_name,first_name])
+
+                    except:
+                        if user == "":
+                            user = input("enter a command or 'quit' to quit")
+                            quits()
+            else:
+                print('invalid command')
+        def student_average(param):
+            print(param)
             f = param[1]
             if len(param) != 4:
                 print("Usage: stu <filename> <first name> <last name>")
-                student_average(["stu", r"C:\Users\abdul\Downloads\GCIS.123.600-assignment2-sample.csv", "Altamimi",'Abdullah'])
+                bye()
             elif len(param) == 4:
-                try:
-                    csv_file = open(f)
-                    csv_reader = csv.reader(csv_file)
-                    firstname = param[3]
-                    lastname = param[2]
-                    for row in csv_reader:
-                        try:
-                            while firstname in row:
-                                if lastname in row:
-                                    plot = row[2:]
-                                    if plotter.add_data_point(plot) == True:
-                                        print('Plotting complete. (window may be hidden)')
-                                        break
-                                    elif plotter.add_data_point(plot) == False:
-                                        print('Plot failed. (student not found)')
-                                        break
+                    try:
+                        csv_file = open(f)
+                        csv_reader = csv.reader(csv_file)
+                        firstname = param[3]
+                        lastname = param[2]
+                        for row in csv_reader:
+                            try:
+                                while firstname in row:
+                                    if lastname in row:
+                                        plot = row[2:]
+                                        plotter.init('my graph','x-axis','y-axis')
+                                        plotter.new_series()
+                                        for i in range(len(plot)):
+                                            plotter.add_data_point(plot[i])
+                                            plotter.plot()
+                                        if plotter.plot() == True:
+                                            print('Plotting complete. (window may be hidden)')
+                                            break
+                                        elif plotter.plot() == False:
+                                            print('Plot failed. (student not found)')
+                                            break
 
-                        except:
-                            print('name not found in csv file')
-                            break
-
-
-
-                except:
-                    print('file does not exist')
-
-            else:
-                print('file does not exist')
-            if plotter.plot(param) == True:
-                    print('Plot finished, window may be hidden.')
-            elif plotter.plot(param) == False:
-                    print('Plot failed, no such student.')
+                            except:
+                                print('name not found in csv file')
+                    except:
+                        print('file not found')
 
 
 
-    def print_average(string):
-            string = input('enter ')
 
-            #quit function
-    def quits():
-            sure = input("are you sure?")
-            while sure == 'y':
-                print('goodbye!')
-                return True
-            while sure == 'Y':
-                print('goodbye!')
-                return True
-            else:
-                return False
-    def bye():
-        user = input(">>")
-        string = user.split()
-        try:
-            if string[0] == "quit":
-                while quits() == True:
-                    break
-            if string[0] == 'stu':
-                    student_average(["stu",r"C:\Users\abdul\Downloads\GCIS.123.600-assignment2-sample.csv","Altamimi","Abdullah"])
-        except:
-            if user == "":
-                user = input("enter a command or 'quit' to quit")
-                quits()
-    bye()
+        bye()
 
 
-main()
+
+if __name__ == '__main__':
+    main()
+
 
 
 
